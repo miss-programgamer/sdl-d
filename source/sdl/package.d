c239ba47b7fcd558f5aea2f75c6fe61075ccc8bf
LunaTheFoxgirl <cli-luna@protonmail.com>
2025-02-06 04:06:52 +0100

    Auto import core sdl functions with sdl module.

--- source/sdl/package.d
+++ source/sdl/package.d
@@ -1,6 +1,9 @@
 /**
     Bindings to SDL3.
 
+    Importing this module imports all of the core functionality of SDL3,
+    namely window and event handling.
+
     Copyright: © 2025 Inochi2D Project, © 1997-2025 Sam Lantinga
     License: Subject to the terms of the Zlib License, as written in the LICENSE file.
     Authors: 
@@ -8,3 +11,18 @@
 */
 module sdl;
 
+public import sdl.init;
+public import sdl.types;
+public import sdl.events;
+public import sdl.video;
+public import sdl.scancode;
+public import sdl.keycode;
+public import sdl.keyboard;
+public import sdl.mouse;
+public import sdl.joystick;
+public import sdl.power;
+public import sdl.audio;
+public import sdl.camera;
+public import sdl.touch;
+public import sdl.pen;
+public import sdl.sensor;
