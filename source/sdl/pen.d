ec231deca7dc696d9e3bd9faec52f66c25b0d1ae
LunaTheFoxgirl <cli-luna@protonmail.com>
2025-02-16 00:51:25 +0100

    Fix sdl.pen docs

--- source/sdl/pen.d
+++ source/sdl/pen.d
@@ -57,56 +57,112 @@ import sdl.stdc;
 extern(C) nothrow @nogc:
 
 /**
-* SDL pen instance IDs.
-*
-* Zero is used to signify an invalid/null device.
-*
-* These show up in pen events when SDL sees input from them. They remain
-* consistent as long as SDL can recognize a tool to be the same pen; but if a
-* pen physically leaves the area and returns, it might get a new ID.
-*
-* \since This datatype is available since SDL 3.2.0.
+    SDL pen instance IDs.
+
+    Zero is used to signify an invalid/null device.
+
+    These show up in pen events when SDL sees input from them. They remain
+    consistent as long as SDL can recognize a tool to be the same pen; but if a
+    pen physically leaves the area and returns, it might get a new ID.
 */
 alias SDL_PenID = Uint32;
 
 /**
-* Pen input flags, as reported by various pen events' `pen_state` field.
-*
-* \since This datatype is available since SDL 3.2.0.
+    Pen input flags, as reported by various pen events' `pen_state` field.
 */
 enum SDL_PenInputFlags : Uint32 {
-    SDL_PEN_INPUT_DOWN = (1u << 0), /**< pen is pressed down */
-    SDL_PEN_INPUT_BUTTON_1 = (1u << 1), /**< button 1 is pressed */
-    SDL_PEN_INPUT_BUTTON_2 = (1u << 2), /**< button 2 is pressed */
-    SDL_PEN_INPUT_BUTTON_3 = (1u << 3), /**< button 3 is pressed */
-    SDL_PEN_INPUT_BUTTON_4 = (1u << 4), /**< button 4 is pressed */
-    SDL_PEN_INPUT_BUTTON_5 = (1u << 5), /**< button 5 is pressed */
-    SDL_PEN_INPUT_ERASER_TIP = (1u << 30), /**< eraser tip is used */
-
+    
+    /**
+        Pen is pressed down
+    */
+    SDL_PEN_INPUT_DOWN = (1u << 0),
+    
+    /**
+        Button 1 is pressed
+    */
+    SDL_PEN_INPUT_BUTTON_1 = (1u << 1),
+    
+    /**
+        Button 2 is pressed
+    */
+    SDL_PEN_INPUT_BUTTON_2 = (1u << 2),
+    
+    /**
+        Button 3 is pressed
+    */
+    SDL_PEN_INPUT_BUTTON_3 = (1u << 3),
+    
+    /**
+        Button 4 is pressed
+    */
+    SDL_PEN_INPUT_BUTTON_4 = (1u << 4),
+    
+    /**
+        Button 5 is pressed
+    */
+    SDL_PEN_INPUT_BUTTON_5 = (1u << 5),
+    
+    /**
+        Eraser tip is used
+    */
+    SDL_PEN_INPUT_ERASER_TIP = (1u << 30),
 }
 
 /**
-* Pen axis indices.
-*
-* These are the valid values for the `axis` field in SDL_PenAxisEvent. All
-* axes are either normalised to 0..1 or report a (positive or negative) angle
-* in degrees, with 0.0 representing the centre. Not all pens/backends support
-* all axes: unsupported axes are always zero.
-*
-* To convert angles for tilt and rotation into vector representation, use
-* SDL_sinf on the XTILT, YTILT, or ROTATION component, for example:
-*
-* `SDL_sinf(xtilt * SDL_PI_F / 180.0)`.
-*
-* \since This enum is available since SDL 3.2.0.
+    Pen axis indices.
+
+    These are the valid values for the `axis` field in SDL_PenAxisEvent. All
+    axes are either normalised to 0..1 or report a (positive or negative) angle
+    in degrees, with 0.0 representing the centre. Not all pens/backends support
+    all axes: unsupported axes are always zero.
+
+    To convert angles for tilt and rotation into vector representation, use
+    SDL_sinf on the XTILT, YTILT, or ROTATION component.
+
+    Example:
+        ---
+        sinf(xtilt * PI / 180.0);
+        ----
 */
 enum SDL_PenAxis {
-    SDL_PEN_AXIS_PRESSURE, /**< Pen pressure.  Unidirectional: 0 to 1.0 */
-    SDL_PEN_AXIS_XTILT, /**< Pen horizontal tilt angle.  Bidirectional: -90.0 to 90.0 (left-to-right). */
-    SDL_PEN_AXIS_YTILT, /**< Pen vertical tilt angle.  Bidirectional: -90.0 to 90.0 (top-to-down). */
-    SDL_PEN_AXIS_DISTANCE, /**< Pen distance to drawing surface.  Unidirectional: 0.0 to 1.0 */
-    SDL_PEN_AXIS_ROTATION, /**< Pen barrel rotation.  Bidirectional: -180 to 179.9 (clockwise, 0 is facing up, -180.0 is facing down). */
-    SDL_PEN_AXIS_SLIDER, /**< Pen finger wheel or slider (e.g., Airbrush Pen).  Unidirectional: 0 to 1.0 */
-    SDL_PEN_AXIS_TANGENTIAL_PRESSURE, /**< Pressure from squeezing the pen ("barrel pressure"). */
-    SDL_PEN_AXIS_COUNT /**< Total known pen axis types in this version of SDL. This number may grow in future releases! */
+    
+    /**
+        Pen pressure.  Unidirectional: 0 to 1.0
+    */
+    SDL_PEN_AXIS_PRESSURE,
+    
+    /**
+        Pen horizontal tilt angle.  Bidirectional: -90.0 to 90.0 (left-to-right).
+    */
+    SDL_PEN_AXIS_XTILT,
+    
+    /**
+        Pen vertical tilt angle.  Bidirectional: -90.0 to 90.0 (top-to-down).
+    */
+    SDL_PEN_AXIS_YTILT,
+    
+    /**
+        Pen distance to drawing surface.  Unidirectional: 0.0 to 1.0
+    */
+    SDL_PEN_AXIS_DISTANCE,
+    
+    /**
+        Pen barrel rotation.  Bidirectional: -180 to 179.9 (clockwise, 0 is facing up, -180.0 is facing down).
+    */
+    SDL_PEN_AXIS_ROTATION,
+    
+    /**
+        Pen finger wheel or slider (e.g., Airbrush Pen).  Unidirectional: 0 to 1.0
+    */
+    SDL_PEN_AXIS_SLIDER,
+    
+    /**
+        Pressure from squeezing the pen ("barrel pressure").
+    */
+    SDL_PEN_AXIS_TANGENTIAL_PRESSURE,
+    
+    /**
+    Total known pen axis types in this version of SDL. This number may grow in future releases!
+    */
+    SDL_PEN_AXIS_COUNT,
 }
